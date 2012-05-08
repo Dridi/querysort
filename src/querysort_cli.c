0f6a034a2441c9136e92605bea1aa370a0acc23d
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2012-05-08 13:23:05 +0200

    added a CLI program for querystring sorting

---
+++ src/querysort_cli.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+#include "querysort.c"
+
+int
+main(const int argc, const char *argv[])
+{
+	if (argc != 2) {
+		printf("Usage : %s URL\n", argv[0]);
+		return 1;
+	}
+
+	// TODO validate URL
+
+	char *url = querysort(argv[1]);
+
+	if (url) {
+		puts(url);
+		free(url);
+	}
+	else {
+		puts("Couldn't create the sorted URL");
+	}
+
+	return 0;
+}
+
